da595c21ecf819ed9feb5b67d554fbc264eeab39
crispimUchoa <francisco,crispim@aluno.uece.br>
2024-02-23 15:12:14 -0300

    feito resource da tabela de categorias

--- src/database/migrations/20240223174050-create-categories-table.js
+++ src/database/migrations/20240223174050-create-categories-table.js
@@ -2,20 +2,35 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.DataTypes.INTEGER
+      },
+      name: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.DataTypes.STRING
+      },
+      position: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.DataTypes.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE
+      }
+    })
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('categories');
   }
 };
