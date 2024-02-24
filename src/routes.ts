01a4f1cd176770a5ba4fb226f5c684b56f08f3ae
crispimUchoa <francisco,crispim@aluno.uece.br>
2024-02-24 17:04:36 -0300

    obtendo informações do curso

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import { categoriesController } from './controllers/categoriesController'
+import { coursesController } from './controllers/coursesController'
 
 export const router = express.Router()
 
 router.get('/categories', categoriesController.index)
 router.get('/categories/:id', categoriesController.show)
+router.get('/courses/:id', coursesController.show)
