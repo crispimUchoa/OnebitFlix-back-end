c385649fdd529b47712ed9a9841fa560c5614e15
crispimUchoa <francisco,crispim@aluno.uece.br>
2024-02-24 16:37:49 -0300

    obtendo cursos de uma categoria

--- src/routes.ts
+++ src/routes.ts
@@ -4,3 +4,4 @@ import { categoriesController } from './controllers/categoriesController'
 export const router = express.Router()
 
 router.get('/categories', categoriesController.index)
+router.get('/categories/:id', categoriesController.show)
