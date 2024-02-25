5ad050f7e52d89217418d29f9bdf726dbabc4bd5
crispimUchoa <francisco,crispim@aluno.uece.br>
2024-02-25 16:18:18 -0300

    adicionando favoritos (minha lista)

---
+++ src/services/favoriteServices.ts
@@ -0,0 +1,11 @@
+import { Favorite } from "../models"
+
+export const favoriteServices = {
+    create: async (userId: number, courseId: number)=>{
+        const favorite = Favorite.create({
+            userId,
+            courseId
+        })
+        return favorite
+    }
+}
