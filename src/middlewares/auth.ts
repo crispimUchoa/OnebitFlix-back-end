86dd2ef1fbf87c85a132dc3498cf0edc0af3e57d
crispimUchoa <francisco,crispim@aluno.uece.br>
2024-02-25 15:15:22 -0300

    protegendo videos

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { jwtServices } from "../services/jwtServices";
 import { userServices } from "../services/userServices";
 import { JwtPayload } from "jsonwebtoken";
-import { UserInstance } from "../models/User";
+import { User, UserInstance } from "../models/User";
 
 export interface AuthenticatedRequest extends Request {
     user?: UserInstance | null
@@ -16,14 +16,32 @@ export function ensureAuth(req: AuthenticatedRequest, res: Response, next: NextF
 
     const token = authorizationHeader.replace(/Bearer/, '').trim()
 
-    jwtServices.verifyToken(token, (err, decoded) => {
+    jwtServices.verifyToken(token, async (err, decoded) => {
         if(err || typeof decoded === 'undefined') return res.status(401).json({
             message: 'Não autorizado: token inválido.'
         })
 
-        userServices.findByEmail((decoded as JwtPayload).email).then(user=>{
+        const user = await userServices.findByEmail((decoded as JwtPayload).email)
             req.user = user
             next()
+
+    })
+}
+
+export function ensureAuthViaQuery(req:AuthenticatedRequest, res: Response, next: NextFunction){
+    const {token} = req.query
+
+    if(!token) return res.status(401).json({message: 'Não autorizado: nenhum token foi encontrado.'})
+
+    if(typeof token !== 'string') return res.status(400).json({message: 'Token deve ser do tipo string.'})
+
+    jwtServices.verifyToken(token, async (err, decoded) => {
+        if(err || typeof decoded === 'undefined') return res.status(401).json({
+            message: 'Não autorizado: token inválido.'
         })
+
+        const user = await userServices.findByEmail((decoded as JwtPayload).email)
+        req.user = user
+        next()
     })
 }
