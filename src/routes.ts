3eae8579b361ca58ea92f03b588d9c05d863bde2
crispimUchoa <francisco,crispim@aluno.uece.br>
2024-02-24 21:23:19 -0300

    obtendo lançamentos

--- src/routes.ts
+++ src/routes.ts
@@ -8,4 +8,5 @@ router.get('/categories', categoriesController.index)
 router.get('/categories/:id', categoriesController.show)
 
 router.get('/courses/featured', coursesController.featured)
+router.get('/courses/newest', coursesController.newest)
 router.get('/courses/:id', coursesController.show)
