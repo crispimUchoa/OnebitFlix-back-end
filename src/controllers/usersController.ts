58e5b9cce873e1854898b2a3dfebe89127116a45
crispimUchoa <francisco,crispim@aluno.uece.br>
2024-02-26 15:43:42 -0300

    obtendo informações do usuário.

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -3,10 +3,24 @@ import { AuthenticatedRequest } from "../middlewares/auth";
 import { userServices } from "../services/userServices";
 
 export const usersController = {
+    //GET /user/current
+    show: async (req: AuthenticatedRequest, res: Response) => {
+         try {
+            const currentUser = req.user!
+            return res.json(currentUser)
+        } catch (err) {
+            if (err instanceof Error) return res.status(400).json({message: err.message})  
+        }
+    },
     //GET /user/current/watching
     watching: async (req: AuthenticatedRequest, res: Response) => {
         const id = req.user!.id
-        const watching = await userServices.getKeepWatchingList(id)
+        
+        try {
+            const watching = await userServices.getKeepWatchingList(id)
         return res.json(watching)
+        } catch (err) {
+            if (err instanceof Error) return res.status(400).json({message: err.message})
+        }
     }
 }
